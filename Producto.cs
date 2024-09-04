dc867d7bbc631509f0e8fcf7b194d60d09b6326d
Ricardito Robinson <locorobinson@hotmail.com>
2024-09-04 16:29:46 -0300

    Agregar archivos de proyecto.

---
+++ Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Ingenierìa_de_software
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public string Categoria { get; set; }
+
+        public Producto(string nombre, decimal precio, string categoria)
+        {
+            Nombre = nombre;
+            Precio = precio;
+            Categoria = categoria;
+        }
+    }
+
+}
---
+++ Tienda.cs
@@ -0,0 +1,34 @@
+﻿namespace Proyecto_Ingenierìa_de_software
+{
+    public class Tienda
+    {
+        private List<Producto> Inventario;
+
+        public Tienda()
+        {
+            Inventario = new List<Producto>();
+        }
+
+        public void AgregarProducto(Producto producto)
+        {
+            Inventario.Add(producto);
+        }
+
+        public Producto BuscarProducto(string nombre)
+        {
+            return Inventario.FirstOrDefault(p => p.Nombre == nombre);
+        }
+
+        public bool EliminarProducto(string nombre)
+        {
+            var producto = BuscarProducto(nombre);
+            if (producto != null)
+            {
+                Inventario.Remove(producto);
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
